602b2a01f0b0ee28cc989fa29e90fef116bbb929
Mouath Ibrahim <Mouath.Ibrahim@gmail.com>
2016-02-01 19:35:06 -0600

    Implement AddFront() & RemoveFront()

--- app/src/main/java/com/example/awesomefat/linkedlist/LinkedList.java
+++ app/src/main/java/com/example/awesomefat/linkedlist/LinkedList.java
@@ -18,19 +18,33 @@ public class LinkedList
 
     public void display()
     {
+
+        //Testing the function
+
+        addFront("MUATH");
+
+        addFront("Android");
+
+        removeFront();
+
+
         if(this.head == null)
         {
-           System.out.println("Empty List");
+           System.out.println("***** Empty List *****");
         }
         else
         {
             //display the list in a reasonable format
         }
+
     }
 
     public void addFront(String value)
     {
         //this adds a new Node to the front of the list with payload == value
+        Node newHead = new Node(value);
+        newHead.setNextNode(this.head);
+        this.head = newHead;
     }
 
     public Node removeFront()
@@ -38,6 +52,14 @@ public class LinkedList
         //this removes and returns the Node that is currently sitting at the
         //front of the list.  The new front of the list, should be the old
         //second node or null in the event it was a 1-list
-        return null;
+        Node nodeToRemove = this.head;
+
+        if(this.head.getNextNode() == null)
+            this.head = null;
+        else
+            this.head = this.head.getNextNode();
+
+
+        return nodeToRemove;
     }
 }
